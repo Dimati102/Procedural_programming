0dc84a901a7bf2ec6fcf8c55c6112834fa7307fb
Dimati102 <114511760+Dimati102@users.noreply.github.com>
2022-10-11 23:05:57 +0300

    Add files via upload

---
+++ 1.2 Arifmetica/1.2 Arifmetica/1.2 Arifmetica.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int x = 1;
+	int y = 1;
+	cin >> x >> y;
+	cout << x + y << endl << x - y << endl << x * y << endl << x / y << endl << x % y;
+}
