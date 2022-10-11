7b7629edbff8b1f20e9432c00355dc9d67c95d1d
Dimati102 <114511760+Dimati102@users.noreply.github.com>
2022-10-11 23:25:18 +0300

    Add files via upload

---
+++ 1.5 LAMP/1.5 LAMP/1.5 LAMP.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	bool lamp, curtain;
+	int day;
+	cout << "Is the lamp on?" << endl;
+	cin >> lamp;
+	cout << "Is curtain opened?" << endl;
+	cin >> curtain;
+	cout << "What time is it?" << endl;
+	cin >> day;
+	if (lamp == 1)
+	{
+		cout << "The room is light.";
+	}
+	else
+	{
+		if ((9 <= day <= 21))
+		{
+			if (curtain == 1)
+			{
+				cout << "The room is light.";
+			}
+			else
+			{
+				cout << "The room is dark.";
+			}
+		}
+		else
+		{
+			cout << "The room is dark.";
+		}
+	}
+}
