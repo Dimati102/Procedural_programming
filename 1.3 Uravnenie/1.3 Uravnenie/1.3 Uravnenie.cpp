b99364db9810470125299a1b4ba5bb37f516273f
Dimati102 <114511760+Dimati102@users.noreply.github.com>
2022-10-11 23:14:42 +0300

    Add files via upload

---
+++ 1.3 Uravnenie/1.3 Uravnenie/1.3 Uravnenie.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double b = 1;
+	double c = 1;
+
+	cin >> b >> c;
+	if (b == 0)
+	{
+		if (c == 0) std::cout << "Х = any number";
+		else cout << "no solutions";
+		return 0;
+	}
+	double x = -c / b;
+	cout << x;
+}
